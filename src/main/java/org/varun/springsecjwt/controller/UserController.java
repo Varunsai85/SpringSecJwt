9685dfc1eb7cdff5815a71b095d77c8bb8d69899
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2025-10-31 01:46:14 +0530

    Added login endpoint

--- src/main/java/org/varun/springsecjwt/controller/UserController.java
+++ src/main/java/org/varun/springsecjwt/controller/UserController.java
@@ -1,6 +1,9 @@
 package org.varun.springsecjwt.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +14,17 @@ import org.varun.springsecjwt.service.UserService;
 @AllArgsConstructor
 public class UserController {
     private UserService service;
+    private AuthenticationManager authenticationManager;
 
     @PostMapping("register")
     public User register(@RequestBody User user){
         return service.addUser(user);
+    }
+
+    @PostMapping("login")
+    public String login(@RequestBody User user){
+        Authentication authentication=authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUserName(),user.getPassword()));
+        if(authentication.isAuthenticated()) return "Success";
+        else return "Failed";
     }
 }
