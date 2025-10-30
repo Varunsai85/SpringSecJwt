eca9af0dce81a30946b2070a6b8fdc135ba9a38f
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2025-10-31 01:40:03 +0530

    Added DI

--- src/main/java/org/varun/springsecjwt/controller/UserController.java
+++ src/main/java/org/varun/springsecjwt/controller/UserController.java
@@ -1,12 +1,14 @@
 package org.varun.springsecjwt.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.varun.springsecjwt.model.User;
-import org.varun.springsecjwt.sevice.UserService;
+import org.varun.springsecjwt.service.UserService;
 
 @RestController
+@AllArgsConstructor
 public class UserController {
     private UserService service;
 
