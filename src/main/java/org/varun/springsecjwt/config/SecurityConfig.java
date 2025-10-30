4b2bd47da625a709b67b719328e4984b24696a5d
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2025-10-31 01:30:02 +0530

    COnfigures Security filter chain

--- src/main/java/org/varun/springsecjwt/config/SecurityConfig.java
+++ src/main/java/org/varun/springsecjwt/config/SecurityConfig.java
@@ -3,8 +3,12 @@ package org.varun.springsecjwt.config;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -14,5 +18,14 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder encoder(){
         return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request->request.requestMatchers("/register").permitAll().anyRequest().authenticated())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 }
