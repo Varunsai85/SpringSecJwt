f51a94433ff3fd682a20110f80d2bc76c43d49ad
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2025-10-31 01:14:11 +0530

    Created User Entity

---
+++ src/main/java/org/varun/springsecjwt/model/User.java
@@ -0,0 +1,19 @@
+package org.varun.springsecjwt.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String userName;
+    private String password;
+}
