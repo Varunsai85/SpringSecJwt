360de9a4b7919c5a2587652b06d5f970b6174a9b
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2025-10-31 01:58:54 +0530

    Added service for generating jwt

---
+++ src/main/java/org/varun/springsecjwt/service/JwtService.java
@@ -0,0 +1,51 @@
+package org.varun.springsecjwt.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+    private final String secret;
+
+    public JwtService(){
+        this.secret=generateSecretKey();
+    }
+
+    public String generateToken(String userName) {
+        Map<String,Object> claims=new HashMap<>();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userName)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+420000))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getKey(){
+        byte[] keyBytes= Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private String generateSecretKey(){
+        try{
+            KeyGenerator keyGen=KeyGenerator.getInstance("HmacSHA256");
+            SecretKey secretKey=keyGen.generateKey();
+            return Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error while generating key: ",e);
+        }
+    }
+}
