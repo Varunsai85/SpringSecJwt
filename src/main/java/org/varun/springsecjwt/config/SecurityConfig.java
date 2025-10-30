49ffe086d7c23f41fa955717661f9ed8e5c65e60
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2025-10-31 01:46:43 +0530

    Configured Authentication Manager bean

--- src/main/java/org/varun/springsecjwt/config/SecurityConfig.java
+++ src/main/java/org/varun/springsecjwt/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package org.varun.springsecjwt.config;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -38,5 +40,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests(request->request.requestMatchers("/register").permitAll().anyRequest().authenticated())
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
