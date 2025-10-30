d6b4776939f763c687bad90ad282b22e865bccff
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2025-10-31 02:21:45 +0530

    Validated token

--- src/main/java/org/varun/springsecjwt/service/JwtService.java
+++ src/main/java/org/varun/springsecjwt/service/JwtService.java
@@ -1,9 +1,11 @@
 package org.varun.springsecjwt.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
@@ -14,6 +16,7 @@ import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -47,5 +50,31 @@ public class JwtService {
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("Error while generating key: ",e);
         }
+    }
+
+    private <T> T extractClaim(String token, Function<Claims,T> claimResolver){
+        Claims claims=extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    public String extractUserName(String token) {
+        return extractClaim(token,Claims::getSubject);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean validateToken(String token, UserDetails userdetails) {
+        String userName=extractUserName(token);
+        return (userName.equals(userdetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractClaim(token,Claims::getExpiration).before(new Date());
     }
 }
