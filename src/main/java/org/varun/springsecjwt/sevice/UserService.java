425438032eaad289397b39cae2957f7c2ead2ecf
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2025-10-31 01:22:44 +0530

    Added UserService

---
+++ src/main/java/org/varun/springsecjwt/sevice/UserService.java
@@ -0,0 +1,19 @@
+package org.varun.springsecjwt.sevice;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.varun.springsecjwt.dao.UserRepo;
+import org.varun.springsecjwt.model.User;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    private UserRepo repo;
+    private BCryptPasswordEncoder encoder;
+
+    public User addUser(User user){
+        user.setPassword(encoder.encode(user.getPassword()));
+        return repo.save(user);
+    }
+}
